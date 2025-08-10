9e056f0cf53da2324df46dcc3e6c571dd14826a3
shunsock <s.tsuchiya.business@gmail.com>
2025-08-10 20:52:45 +0900

    feature: init from template

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "C# Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.dotnet-sdk_9
+            pkgs.go-task
+            pkgs.tree
+          ];
+          DOTNET_ROOT = "${pkgs.dotnet-sdk_9}/share/dotnet";
+          shellHook = ''
+            echo "dotnet version: $(dotnet --version)"
+            echo "task version: $(task --version)"
+            echo "tree version: $(tree --version)"
+            echo "DOTNET_ROOT: $DOTNET_ROOT"
+          '';
+        };
+      });
+}
